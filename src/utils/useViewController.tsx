8cb0681a8d07b071023348f16f4fe35b4644e224
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-02-28 13:02:35 +0330

    feat: added error

--- src/utils/useViewController.tsx
+++ src/utils/useViewController.tsx
@@ -3,6 +3,7 @@ import stringMath from "string-math"
 
 const useViewController = () => {
     const [display, setDisplay] = useState<string>("0")
+    const [error, setError] = useState<boolean>(false)
 
     const handleEquals = () => {
         try {
@@ -10,37 +11,45 @@ const useViewController = () => {
         }
         catch {
             setDisplay("Error")
+            setError(true)
         }
     }
 
     const handleReset = () => {
         setDisplay("0")
+        setError(false)
     }
 
     const handleDelete = () => {
-        if (display.length === 1) {
-            setDisplay("0") 
-        }
-        else {
-            setDisplay(display.slice(0, -1)) 
+        if (error === false) {
+            if (display.length === 1) {
+                setDisplay("0") 
+            }
+            else {
+                setDisplay(display.slice(0, -1)) 
+            }    
         }
     }
 
     const handleOperation = (sign : string) => {
-        if (sign != display[display.length - 1]) {
-            setDisplay(display.concat(sign))
-        }
-        if (sign === "." && isNaN(Number(display[display.length]))) {
-            setDisplay(display.concat("0."))
+        if (error === false) {
+            if (sign != display[display.length - 1]) {
+                setDisplay(display.concat(sign))
+            }
+            if (sign === "." && isNaN(Number(display[display.length]))) {
+                setDisplay(display.concat("0."))
+            }   
         }
     }
 
     const handleNumInput = (num : string) => {
-        if (display === "0") {
-            setDisplay(num)
-        }
-        else {
-            setDisplay(display + num)
+        if (error === false) {
+            if (display === "0") {
+                setDisplay(num)
+            }
+            else {
+                setDisplay(display + num)
+            }    
         }
     }
 
