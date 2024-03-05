f4a4692b3a724c08f31333cb9764b54b395291b7
B4rsam <105477761+B4rsam@users.noreply.github.com>
2024-03-05 16:17:35 +0330

    feat: added theme button transition

--- src/components/ThemeButton/ThemeButton.tsx
+++ src/components/ThemeButton/ThemeButton.tsx
@@ -5,10 +5,28 @@ interface ITheme {
     themeChange : () => void
 }
 const ThemeButton : FC<ITheme>= ({themeChange}) => {
+    const handleTheme = () => {
+        const element = document.querySelector("span")
+        const state = element?.getAttribute("data-toggle-state")
+
+        switch(state) {
+            case "1":
+                element?.setAttribute("data-toggle-state", "2")
+                break;
+            case "2":
+                element?.setAttribute("data-toggle-state", "3")
+                break;
+            case "3":
+                element?.setAttribute("data-toggle-state", "1")
+                break;
+        }
+
+        themeChange()
+    }
     return (
         <label className={s.outerLabel}>
-            <button className={s.button} onClick={themeChange}></button>
-            <span className={s.innerLabel}></span>
+            <button className={s.button} onClick={handleTheme}></button>
+            <span data-toggle-state={"1"} className={s.innerLabel}></span>
         </label>
     )
 }
